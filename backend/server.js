15135ac006f903bcec036eaab2c9ec43a0dda90f
Fikriraihan <fikri.raihan4@gmail.com>
2024-05-30 17:51:24 +0700

    add port variable

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+const PORT = process.env.PORT || 8000;
+
 connectDB();
 
 const app = express();
@@ -21,6 +23,6 @@ app.get("/", (req, res) => {
 app.use(errorHandler);
 app.use(notFound);
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log("Listening on port 8000");
 });
