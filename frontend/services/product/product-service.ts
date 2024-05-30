e372af32be6be18bb3aeffd4e2a20afb5d629edf
Fikriraihan <fikri.raihan4@gmail.com>
2024-05-30 17:42:37 +0700

    add vercel.json backend

--- frontend/services/product/product-service.ts
+++ frontend/services/product/product-service.ts
@@ -1,17 +1,20 @@
+// frontend/services/api.js
 "use server";
 import { unstable_noStore as noStore, revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export const getAllProducts = async () => {
   noStore();
-  return await fetch("http://localhost:8000/api/products").then((res) => res.json());
+  return await fetch(`${API_BASE_URL}/api/products`).then((res) => res.json());
 };
 
 export const createProduct = async (product: FormData) => {
   const rawData = Object.fromEntries(product);
 
   try {
-    await fetch("http://localhost:8000/api/products", {
+    await fetch(`${API_BASE_URL}/api/products`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
