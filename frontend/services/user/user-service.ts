fab91cdc70f5bad9b66fb097b84fb780dae55ea5
Fikriraihan <fikri.raihan4@gmail.com>
2024-05-31 10:45:41 +0700

    add base url user service

--- frontend/services/user/user-service.ts
+++ frontend/services/user/user-service.ts
@@ -1,5 +1,7 @@
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export const getAllUsers = async () => {
-  const users = await fetch("http://localhost:8000/api/users");
+  const users = await fetch(`${API_BASE_URL}/api/users`);
   const data = await users.json();
   return data;
 };
